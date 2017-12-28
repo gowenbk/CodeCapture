0925ba7db118aaf373a77447f21e27a80030f21f
gowenbk <echizen620@gmail.com>
2017-12-28 15:59:02 +0900

    Login Page Completed!

---
+++ app/src/main/java/com/example/scmaster/codecapture/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.scmaster.codecapture;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by scmaster on 2017-12-28.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
