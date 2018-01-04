fb501de6f5cd840f4ccfaa8704868e261f980f0b
casteraninza <casteraninza@gmail.com>
2018-01-04 11:27:04 +0900

    This is YooBeenChang HAHAHA

--- app/src/main/java/com/example/scmaster/codecapture/MainActivity.java
+++ app/src/main/java/com/example/scmaster/codecapture/MainActivity.java
@@ -1,13 +1,123 @@
 package com.example.scmaster.codecapture;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView listItemView;
+    static final String[] CODE_LIST = {"code1", "code2", "code3", "code4", "code5", "code6", "code7", "code8", "code9", "code10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //ListView
+        listItemView = (ListView)findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2, android.R.id.text1, CODE_LIST);
+        listItemView.setAdapter(adapter);
+
+        //Circle Float Action Button
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        ImageView itemIconShare = new ImageView(this);
+        itemIconShare.setImageResource(R.drawable.fba_sharebtn);
+        SubActionButton shareBtn = itemBuilder.setContentView(itemIconShare).build();
+
+        ImageView itemIconEdit = new ImageView(this);
+        itemIconEdit.setImageResource(R.drawable.fba_editbtn);
+        SubActionButton editBtn = itemBuilder.setContentView(itemIconEdit).build();
+
+        ImageView itemIconDelete = new ImageView(this);
+        itemIconDelete.setImageResource(R.drawable.fba_deletebtn);
+        SubActionButton deleteBtn = itemBuilder.setContentView(itemIconDelete).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(shareBtn)
+                .addSubActionView(editBtn)
+                .addSubActionView(deleteBtn)
+                .attachTo(fab)
+                .build();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_add) {
+            // Handle the camera action
+        } else if (id == R.id.nav_calendar) {
+
+        } else if (id == R.id.nav_folder1) {
+
+        } else if (id == R.id.nav_folder2) {
+
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
