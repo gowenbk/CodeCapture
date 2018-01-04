b1271451231f2c2ca035df04417b570ae868776c
casteraninza <casteraninza@gmail.com>
2018-01-04 11:30:35 +0900

    This is YooBeenChang HAHAHA + unversioned

---
+++ app/src/main/java/com/example/scmaster/codecapture/ManageInfoActivity.java
@@ -0,0 +1,19 @@
+package com.example.scmaster.codecapture;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SCMaster on 2018-01-04.
+ */
+
+public class ManageInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manageinfo);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/scmaster/codecapture/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.scmaster.codecapture;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SCMaster on 2018-01-04.
+ */
+
+public class RegisterActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
